8ad65d81c1bd2117acd65c103b719d1937d1547f
sky-min <ppt00003333@gmail.com>
2021-04-03 05:10:34 +0000

    lulalula

--- level.py
+++ level.py
@@ -1,12 +1,42 @@
 lv = 1
 exp = 100
-plus = 110/100
+plus = 1.5
 maxlev = 200
 
-f = open('10per.txt', 'a')
-print("=====10%=====", file=f)
+f = open('txt/level.txt', 'a')
+print("1~40(5%)", file=f)
+print("41~80(7%)", file=f)
+print("81~120(5%)", file=f)
+print("121~160(7%)", file=f)
+print("161~180(4%)", file=f)
+print("181~200(6%)", file=f)
 
-while lv < maxlev + 1:
+while lv < 41:
     print(lv, "=", int(exp), file=f)
     lv = lv + 1
-    exp = exp * plus
+    exp = exp * 1.05
+
+while lv < 81:
+    print(lv, "=", int(exp), file=f)
+    lv = lv + 1
+    exp = exp * 1.07
+
+while lv < 121:
+    print(lv, "=", int(exp), file=f)
+    lv = lv + 1
+    exp = exp * 1.05
+
+while lv < 161:
+    print(lv, "=", int(exp), file=f)
+    lv = lv + 1
+    exp = exp * 1.07
+
+while lv < 181:
+    print(lv, "=", int(exp), file=f)
+    lv = lv + 1
+    exp = exp * 1.04
+
+while lv < 201:
+    print(lv, "=", int(exp), file=f)
+    lv = lv + 1
+    exp = exp * 1.06
